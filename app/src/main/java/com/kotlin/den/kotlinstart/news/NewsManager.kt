338a6e42e0dc82a507dc436d1c3537813d2e956e
Denis <denisdenisdenis@bigmir.net>
2017-01-30 00:47:23 +0200

    Chapter 7

--- app/src/main/java/com/kotlin/den/kotlinstart/news/NewsManager.kt
+++ app/src/main/java/com/kotlin/den/kotlinstart/news/NewsManager.kt
@@ -1,27 +1,36 @@
 package com.kotlin.den.kotlinstart.news
 
 import com.kotlin.den.kotlinstart.api.RestApi
+import com.kotlin.den.kotlinstart.common.RedditNews
 import com.kotlin.den.kotlinstart.common.RedditNewsItem
 import rx.Observable
 
 /**
+ * NewsManager allows you to request news from Reddit.
  * Created by Denis on 29-Jan-17.
  */
+
 class NewsManager(private val api: RestApi = RestApi()) {
 
-    fun getNews(limit: String = "10"): Observable<List<RedditNewsItem>> {
+    fun getNews(after: String, limit: String = "10"): Observable<RedditNews> {
         return Observable.create {
             subscriber ->
-            val callResponse = api.getNews("", limit)
+            val callResponse = api.getNews(after, limit)
             val response = callResponse.execute()
 
             if (response.isSuccessful) {
-                val news = response.body().data.children.map {
+                val dataResponse = response.body().data
+                val news = dataResponse.children.map {
                     val item = it.data
                     RedditNewsItem(item.author, item.title, item.num_comments,
                             item.created, item.thumbnail, item.url)
                 }
-                subscriber.onNext(news)
+                val redditNews = RedditNews(
+                        dataResponse.after ?: "",
+                        dataResponse.before ?: "",
+                        news
+                )
+                subscriber.onNext(redditNews)
                 subscriber.onCompleted()
             } else {
                 subscriber.onError(Throwable(response.message()))
